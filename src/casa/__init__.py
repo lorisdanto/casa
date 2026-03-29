d20d4402bbdd67a2a57d71e52a894fcc48a4ddb5
sairam2661 <sairamvaidya2661@gmail.com>
2026-03-29 14:22:04 -0700

    update API's we expose

--- src/casa/__init__.py
+++ src/casa/__init__.py
@@ -1,6 +1,7 @@
 from casa.llm import LLM
+from casa.backends import TransformersBackend, VLLMBackend
 from casa.grammar import Grammar
-from casa.samplers.rejection import RS, ARS, RSFT, CARS
+from casa.samplers import RS, ARS, RSFT, CARS
 from casa.samplers.mcmc import MCMC
 
 __version__ = "0.1.0"
@@ -13,4 +14,6 @@ __all__ = [
     "RSFT",
     "CARS",
     "MCMC",
+    "TransformersBackend",
+    "VLLMBackend",
 ]
